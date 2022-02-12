62658f7fd9333448312b418bae5301719cb7ec03
Usu2 Name <usu2@example.com>
2022-02-12 21:38:27 +0100

    elimino

--- prueba1/src/cambio1/Cambio2.java
+++
@@ -1,8 +0,0 @@
-package cambio1;
-
-public class Cambio2 {
-	public static void main(String[] args) {
-		System.out.println("Clase cambio2 en proyecto2.");
-		System.out.println("Clase cambio2 en proyecto2.");
-	}
-}
